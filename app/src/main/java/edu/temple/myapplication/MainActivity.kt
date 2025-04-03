81be4316817f1063c5ccff3b661ac4f1d0a825e7
Jonathan Cherry <tuo97200@temple.edu>
2025-04-03 12:08:40 -0400

    Button logic & Handler creation

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,21 +1,58 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
+import android.widget.TextView
+import java.util.logging.Logger
 
 class MainActivity : AppCompatActivity() {
+    lateinit var timerBinder:TimerService.TimerBinder
+    var isConnected = false
+    lateinit var textView: TextView
+
+    val serviceObject = object : ServiceConnection {
+        override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+            timerBinder = p1 as TimerService.TimerBinder
+            timerBinder.setHandler(handler)
+            isConnected = true
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            isConnected = false
+        }
+    }
+
+    val handler= Handler(Looper.getMainLooper()){
+        textView.text = it.what.toString()
+        true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.textView)
+
+        bindService(
+            Intent(this, TimerService::class.java),
+            serviceObject,
+            BIND_AUTO_CREATE
+        )
 
         findViewById<Button>(R.id.startButton).setOnClickListener {
-
+            if(isConnected)timerBinder.start(
+                100
+            )
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            timerBinder?.stop()
         }
     }
 }
